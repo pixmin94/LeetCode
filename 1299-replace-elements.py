6de5fe952fef82ee1a388d83badeb797db0cf8d3
corina <pixmin@outlook.com>
2023-01-21 17:45:07 +0800

    O(n) solution for challenge 1299

--- 1299-replace-elements.py
+++ 1299-replace-elements.py
@@ -3,12 +3,25 @@
 
 # After doing so, return the array.
 
+# O(n^2):
+# class Solution:
+#     def replaceElements(self, arr: List[int]) -> List[int]:
+#         for index,var in enumerate(arr):
+#             newarr = arr[index+1:]
+#             if len(newarr) == 0:
+#                 arr[index] = -1
+#             else:
+#                 arr[index] = max(newarr)
+#         return arr
+
+# O(n):
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        for index,var in enumerate(arr):
-            newarr = arr[index+1:]
-            if len(newarr) == 0:
-                arr[index] = -1
-            else:
-                arr[index] = max(newarr)
+        rightMax = -1
+
+        for i in range(len(arr) - 1, -1, -1):
+            newMax = max(rightMax, arr[i])
+            arr[i] = rightMax
+            rightMax = newMax
         return arr
+
