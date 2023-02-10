90e47b85569c710fb5840b465fc501fd95af4332
corina <pixmin@outlook.com>
2023-02-10 22:41:56 +0800

    added group anagram challenge

---
+++ Arrays & Hashing/49-group-anagrams.py
@@ -0,0 +1,17 @@
+# Given an array of strings strs, group the anagrams together. 
+# You can return the answer in any order.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+# typically using all the original letters exactly once.
+
+# Solution:
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
